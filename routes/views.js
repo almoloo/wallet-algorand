604c647a51bbac128a135d7a2cbeca5160f4165c
Ali Mousavi <amousavig@icloud.com>
2022-01-02 09:04:05 +0330

    Changed authentication from header to cookie.

--- routes/views.js
+++ routes/views.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const router = express.Router()
+const cookieParser = require('cookie-parser')
 
 // ----- Welcome
 router.get('/', (req, res) => {
-	res.render('welcome')
+	const accountCookie = req.cookies.account
+	if (accountCookie) {
+	} else {
+		res.render('welcome')
+	}
 })
 
 // ----- Login
